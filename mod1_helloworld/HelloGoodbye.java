30f9f6d193a85ccd9fdbf80ccbbbacb2793dda0d
AlanOng2004 <alancx73@gmail.com>
2025-05-12 14:44:14 +0800

    Add HelloWorld.java and update HelloGoodbye.java

--- mod1_helloworld/HelloGoodbye.java
+++ mod1_helloworld/HelloGoodbye.java
@@ -1,1 +1,7 @@
-
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        if (args.length == 2){
+            System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+        }
+    }
+}

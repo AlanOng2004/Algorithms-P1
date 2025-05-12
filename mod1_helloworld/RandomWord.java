f3326d66cc2d3b948bf61dff051828ba01425beb
AlanOng2004 <alancx73@gmail.com>
2025-05-12 15:40:32 +0800

    Added RandomWord.java and fixed HelloGoodbye.java

---
+++ mod1_helloworld/RandomWord.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord{
+    public static void main(String[] args) {
+        int i = 1;
+        String result = "";
+        while (!StdIn.isEmpty())
+        {
+            String next = StdIn.readString();
+            if (StdRandom.bernoulli(1/ i))
+            {
+                result = next;
+            }
+            i++;
+        }
+        System.out.println(result);
+    }
+}
